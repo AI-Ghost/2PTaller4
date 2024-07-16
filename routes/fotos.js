8818749481af49468d255cdc61c20e84b96f2fe3
Ghost <diana30mayo@gmail.com>
2024-07-16 10:35:19 -0500

    commit

--- routes/fotos.js
+++ routes/fotos.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-const Sequelize = require('sequelize');
+const { Sequelize, Op } = require('sequelize');
 const Foto = require('../models').foto;
 const Etiqueta = require('../models').etiqueta;
 
@@ -35,6 +35,55 @@ router.get('/findAll/view', function (req, res, next) {
             res.render('fotos', { title: 'Fotos', arrFotos: fotos });
         })
         .catch(error => res.status(400).send(error))
+});
+
+router.get('/findAllByRate/json',
+    function (req, res, next) {
+        let lower = parseFloat(req.query.lower);
+        let higher = parseFloat(req.query.higher);
+
+        Foto.findAll({
+            attributes: { exclude:["updatedAt"] } ,
+            include: [{
+                model: Etiqueta,
+                attributes: ['texto'],
+                through: {attributes: []}
+            }],
+            where: {
+                calificacion: {
+                [Op.between]: [lower, higher]
+                }
+            }
+        })
+
+            .then(fotos => {
+                res.json(fotos);
+            })
+            .catch(error => res.status(400).send(error))
+    });
+
+router.get('/findAllById/:id/json',
+    function (req, res, next) {
+        let id = parseInt(req.params.id);
+
+        Foto.findAll({
+            attributes: { exclude: ["updatedAt"]} ,
+            include: [{
+                model: Etiqueta,
+                attributes: ['texto'],
+                through: {attributes: []}
+            }],
+            where: {
+            [Op.and]: [{id: id}]
+            }
+            })
+
+            .then(fotos => {
+            res.render('vista', { title: 'Vista', arrFotos: fotos });
+            })
+            .catch(error =>
+            res.status(400).send(error))
+
 });
 
 module.exports = router;
